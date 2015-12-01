5276459c2fdce6a115ba874c06d1aafe497f41e6
Nick Stockton <nstockton@users.noreply.github.com>
2015-12-01 13:01:10 -0800

    Use a sample mushclient_prefs.sqlite file, copying it over the mushclient_prefs.sqlite file when mushclient is run.

--- mc.bat
+++ mc.bat
@@ -1,5 +1,10 @@
 @ECHO OFF
 
+rem Change the current working directory to the location of this batch script.
+pushd "%~dp0"
+copy /Y mushclient_prefs.sqlite.sample mushclient_prefs.sqlite > nul
 start MUSHclient.exe
 cd mapperproxy
 python -B startmapper.py
+rem Reset the working directory to it's previous value, before this batch script was run.
+popd
