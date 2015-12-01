70e7b527f4c9f9c6e25aa240a18e223d6a7f5d3a
Nick Stockton <nstockton@users.noreply.github.com>
2015-12-01 13:21:38 -0800

    Rename MUSHclient.ini to MUSHclient.ini.sample. It will now be copied to MUSHclient.ini each time mushclient loads.

--- mc.bat
+++ mc.bat
@@ -3,6 +3,7 @@
 rem Change the current working directory to the location of this batch script.
 pushd "%~dp0"
 copy /Y mushclient_prefs.sqlite.sample mushclient_prefs.sqlite > nul
+copy /Y MUSHclient.ini.sample MUSHclient.ini > nul
 copy /Y worlds\mume\mume.mcl.sample worlds\mume\mume.mcl > nul
 start MUSHclient.exe
 cd mapperproxy
