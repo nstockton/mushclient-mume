7f7da1a6aceb18a46929a2d49e01e27ee59f5c9f
Nick Stockton <nstockton@users.noreply.github.com>
2015-12-06 22:05:38 -0800

    Only override mume.mcl if it doesn't already exist.

--- mc.bat
+++ mc.bat
@@ -4,7 +4,7 @@ rem Change the current working directory to the location of this batch script.
 pushd "%~dp0"
 copy /Y mushclient_prefs.sqlite.sample mushclient_prefs.sqlite > nul
 copy /Y MUSHclient.ini.sample MUSHclient.ini > nul
-copy /Y worlds\mume\mume.mcl.sample worlds\mume\mume.mcl > nul
+if not exist "worlds\mume\mume.mcl" copy /Y worlds\mume\mume.mcl.sample worlds\mume\mume.mcl > nul
 start MUSHclient.exe
 cd mapperproxy
 python -B startmapper.py
