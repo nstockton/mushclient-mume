8da82a8f6e3b38e0169c38f90bf478d91eca391f
Nick Stockton <nstockton@users.noreply.github.com>
2015-12-26 15:59:34 -0800

    Set up a background world to connect to then Angband chat server.

--- mc.bat
+++ mc.bat
@@ -5,6 +5,7 @@ pushd "%~dp0"
 copy /Y mushclient_prefs.sqlite.sample mushclient_prefs.sqlite > nul
 copy /Y MUSHclient.ini.sample MUSHclient.ini > nul
 if not exist "worlds\mume\mume.mcl" copy /Y worlds\mume\mume.mcl.sample worlds\mume\mume.mcl > nul
+if not exist "worlds\angband\angband.mcl" copy /Y worlds\angband\angband.mcl.sample worlds\angband\angband.mcl > nul
 start MUSHclient.exe
 cd mapperproxy
 python -B startmapper.py
