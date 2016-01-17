47c52b8afdaecb3130f8528487b0b8ab1e84b6e5
Nick Stockton <nstockton@users.noreply.github.com>
2016-01-17 15:09:06 -0800

    In the mc.bat script, first try to load the mapper proxy with PyPy, then Python.

--- mc.bat
+++ mc.bat
@@ -8,6 +8,12 @@ if not exist "worlds\mume\mume.mcl" copy /Y worlds\mume\mume.mcl.sample worlds\m
 if not exist "worlds\angband\angband.mcl" copy /Y worlds\angband\angband.mcl.sample worlds\angband\angband.mcl > nul
 start MUSHclient.exe
 cd mapperproxy
-python -B startmapper.py raw
+for %%x in (pypy.exe) do if not [%%~$PATH:x]==[] echo "Running mapper with PyPy." && pypy.exe -B startmapper.py raw && goto :finished
+for %%x in (python.exe) do if not [%%~$PATH:x]==[] echo "Running mapper with Python." && python.exe -B startmapper.py raw && goto :finished
+rem If we're here, Python wasn't found in the path.
+echo "Error: Unable to locate Python!"
+echo "This script requires that either PyPy or Python be installed and in the path."
+pause
+:finished
 rem Reset the working directory to it's previous value, before this batch script was run.
 popd
