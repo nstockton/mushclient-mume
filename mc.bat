800f534ffe79a471600b9465465c62f21c3d3b99
Nick Stockton <nstockton@users.noreply.github.com>
2019-06-13 13:16:46 -0700

    ***First commit after git rebase.***
    The mapper will no longer be included as a git subtree. Instead, add an update script which downloads the mapper and keeps it up to date with GitHub releases.
    The update script is written in Lua, though for the moment I have decided to use CURL (built as an executable) for downloading the mapper rather than Lua-CURL or LuaSocket with OpenSSL.
    The included curl.exe was compiled by me using MinGW32 in order to reduce file size. The official releases of CURL statically link against OpenSSL and a bunch of other libraries which aren't needed for the purpose of running the update script. Currently, official releases of curl.exe are around 3.5MB in size, while my version which uses the built-in SSL support from Windows is only 800KB. The down side to this is that this version of CURL might fail to download from GitHub on Windows XP or older. If this is the case, the user should download an official release of CURL so that OpenSSL is used rather than WinSSL.
    The update script also makes use of unzip.exe from the Info Zip package for extracting mapper releases. The included unzip.exe was taken from the official release at ftp://ftp.info-zip.org/pub/infozip/win32/unz600xn.exe
    The licenses for CURL and Info Zip can be found in the docs directory of this repository.

--- mc.bat
+++ mc.bat
@@ -1,23 +1,25 @@
 @ECHO OFF
 
+cls
+
 rem Change the current working directory to the location of this batch script.
 pushd "%~dp0"
-copy /Y mushclient_prefs.sqlite.sample mushclient_prefs.sqlite > nul
-copy /Y MUSHclient.ini.sample MUSHclient.ini > nul
-if not exist "worlds\mume\mume.mcl" copy /Y worlds\mume\mume.mcl.sample worlds\mume\mume.mcl > nul
-start MUSHclient.exe
-cd mapperproxy
-for %%x in (python.exe) do (
-	if not [%%~$PATH:x]==[] (
-		echo "Running mapper with Python."
-		python.exe -B start.py --interface hc --format raw
-		goto :finished
-	)
+
+copy /Y mushclient_prefs.sqlite.sample mushclient_prefs.sqlite >nul
+copy /Y MUSHclient.ini.sample MUSHclient.ini >nul
+if not exist "worlds\mume\mume.mcl" copy /Y worlds\mume\mume.mcl.sample worlds\mume\mume.mcl >nul
+
+call update.bat /CalledByScript
+
+if exist "mapper_proxy" (
+	start MUSHclient.exe
+	cd mapper_proxy
+	echo Running the mapper.
+	"Mapper Proxy.exe" --interface hc --format raw
+) else (
+	echo Error: failed to start because the mapper was not found.
+	pause
 )
-rem If we're here, Python wasn't found in the path.
-echo "Error: Unable to locate Python!"
-echo "This script requires that either PyPy or Python be installed and in the path."
-pause
-:finished
+
 rem Reset the working directory to it's previous value, before this batch script was run.
 popd
