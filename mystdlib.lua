0b6284f1c2bd268a5efb1c9d2c20b177edc59e1a
Nick Stockton <nstockton@users.noreply.github.com>
2019-06-30 01:26:12 -0700

    Change the import of lfs in mystdlib.lua to be a local import.

--- mystdlib.lua
+++ mystdlib.lua
@@ -2,7 +2,8 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-require("lfs")
+
+local lfs = require("lfs")
 local sha2 = require("sha2")
 local stringy = require("stringy")
 
