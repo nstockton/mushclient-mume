3487c580b1110327b9753f9f7e50622646ca0565
Nick Stockton <nstockton@users.noreply.github.com>
2019-06-29 19:04:54 -0700

    Implement the update script entirely in Lua. There is still an update.bat file, but its only use is if the user wants to run the updater stand-alone.

--- update.bat
+++ update.bat
@@ -1,34 +1,11 @@
 @echo off
+
 cls
 
 rem Change the current working directory to the location of this batch script.
 pushd "%~dp0"
 
-rem A small delay is needed here so that NVDA will automatically speak the first line of output.
-rem timeout /T 1 /NOBREAK >nul 2>&1
-luajit.exe -e "require('socket'); socket.sleep(0.1)"
-
-if exist "tempmapper" rd /S /Q "tempmapper"
-
-if exist "mapper_proxy" (
-	if "%1" NEQ "/CalledByScript" echo Checking for updates to the mapper.
-) else (
-	echo Mapper Proxy not found. This is normal for new installations.
-)
-
-luajit.exe update_checker.lua
-
-if exist "mapper_proxy.zip" (
-	echo Extracting files.
-	unzip.exe -qq "mapper_proxy.zip" -d "tempmapper"
-	for /D %%G in ("tempmapper\Mapper_Proxy_V*") DO xcopy "%%G" "mapper_proxy" /E /V /I /Q /R /Y
-	rd /S /Q "tempmapper"
-	del /F /Q "mapper_proxy.zip"
-	echo Done.
-	pause
-) else (
-	if "%1" NEQ "/CalledByScript" pause
-)
+luajit.exe update_checker.lua %*
 
 rem Reset the working directory to it's previous value, before this batch script was run.
 popd
